23ec2606f2ac00640e679294dc2616362f805380
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-06-09 22:33:22 -0400

    Added GET to list patients

--- hih.py
+++
@@ -1,2 +0,0 @@
-def say_my_name():
-    print(__name__)
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-
-
-
+import psycopg2
+import json
 
 app = FastAPI()
 
+#Connection to PostgreSQL database
+conn = psycopg2.connect(
+    host="localhost",
+    database="emrapp",
+    user="postgres",
+    password="postgres1")
 
-@app.get("/")
-def read_root():
-    return 'dhfghfh'
+@app.get("api/patients")
+def list_all_patients():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM patient; ")
+    return cursor.fetchall()
